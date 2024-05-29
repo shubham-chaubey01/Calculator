ec524e6ff807b56e6019f093068905e11c59d980
Shubham Chaubey <sc0122757@gmail.com>
2024-05-29 23:36:15 +0530

    calculator

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
+import com.example.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val calculatorViewModal = ViewModelProvider(this)[CalculatorViewModal ::class.java]
+        enableEdgeToEdge()
+        setContent {
+            CalculatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Calculator(modifier = Modifier.padding(innerPadding),calculatorViewModal)
+                }
+            }
+        }
+    }
+}
+
